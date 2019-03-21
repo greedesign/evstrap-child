97c1173c2a7aad3f6437c17d44d5b53011bcf0d9
Graeme Reed <graeme@parcomonline.com>
2019-03-21 16:59:55 -0600

    initial customizations

---
+++ inc/editor.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Understrap modify editor
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Set Child Theme Editor Colour Palette
+ */
+
+if ( ! function_exists( 'understrap_child_setup_theme_supported_features' ) ) {
+	function understrap_child_setup_theme_supported_features() {
+		add_theme_support( 'editor-color-palette', array(
+				array(
+						'name' => __( 'strong magenta', 'themeLangDomain' ),
+						'slug' => 'strong-magenta',
+						'color' => '#a156b4',
+				),
+				array(
+						'name' => __( 'light grayish magenta', 'themeLangDomain' ),
+						'slug' => 'light-grayish-magenta',
+						'color' => '#d0a5db',
+				),
+				array(
+						'name' => __( 'very light gray', 'themeLangDomain' ),
+						'slug' => 'very-light-gray',
+						'color' => '#eee',
+				),
+				array(
+						'name' => __( 'very dark gray', 'themeLangDomain' ),
+						'slug' => 'very-dark-gray',
+						'color' => '#444',
+				),
+		) );
+	}
+}
+
+add_action( 'after_setup_theme', 'understrap_child_setup_theme_supported_features' );
