f337c00e35b470d4da7846175f648ee799404712
Graeme Reed <graeme@parcomonline.com>
2019-06-14 17:09:16 -0600

    cleaned up customizer and editor files

--- inc/child-customizer.php
+++ inc/child-customizer.php
@@ -15,51 +15,15 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 //require_once trailingslashit( dirname(__FILE__) ) . 'custom-controls.php';
 
+// if ( ! function_exists( 'evstrap_child_theme_customize_register' ) ) {
+// 	/**
+// 	 * Register individual settings through customizer's API.
+// 	 *
+// 	 * @param WP_Customize_Manager $wp_customize Customizer reference.
+// 	 */
+// 	function evstrap_child_theme_customize_register( $wp_customize ) {
 
-if ( ! function_exists( 'evstrap_child_theme_customize_register' ) ) {
-	/**
-	 * Register individual settings through customizer's API.
-	 *
-	 * @param WP_Customize_Manager $wp_customize Customizer reference.
-	 */
-	function evstrap_child_theme_customize_register( $wp_customize ) {
 
-		/**
-		 * Theme layout options.
-		 */ 
-		$wp_customize->add_section(
-			'evstrap_child_ticket_options',
-			array(
-				'title'       => __( 'BVJ Ticket Settings', 'evstrap' ),
-				'capability'  => 'edit_theme_options',
-				'description' => __( 'Container width and sidebar defaults', 'evstrap' ),
-			)
-		);
-
-			// Container Type
-			$wp_customize->add_setting(
-				'evstrap_child_buy_tickets_url',
-				array(
-					'default'           => '',
-					'type'              => 'theme_mod',
-					//'sanitize_callback' => 'evstrap_switch_sanitization',
-					'capability'        => 'edit_theme_options',
-				)
-			);
-			$wp_customize->add_control(
-				new WP_Customize_Control(
-					$wp_customize,
-					'evstrap_child_buy_tickets_url',
-					array(
-						'label'       => __( 'BVJ Festival Ticketing URL ', 'evstrap_child' ),
-						'description' => __( 'Add the Festival Ticketing URL here to set the destination for msot Buy Tickets links across the site', 'evstrap_child' ),
-						'section'     => 'evstrap_child_ticket_options',
-						'options'    => 'evstrap_child_buy_tickets_url',
-						'type'        => 'url',
-					)
-				)
-			);
-
-	}
-} // endif function_exists( 'evstrap_child_theme_customize_register' ).
-add_action( 'customize_register', 'evstrap_child_theme_customize_register' );
+// 	}
+// } // endif function_exists( 'evstrap_child_theme_customize_register' ).
+// add_action( 'customize_register', 'evstrap_child_theme_customize_register' );
--- inc/editor.php
+++ inc/editor.php
@@ -13,31 +13,31 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Set Child Theme Editor Colour Palette
  */
 
-if ( ! function_exists( 'evstrap_child_setup_theme_supported_features' ) ) {
-	function evstrap_child_setup_theme_supported_features() {
-		add_theme_support( 'editor-color-palette', array(
-				array(
-						'name' => __( 'strong magenta', 'themeLangDomain' ),
-						'slug' => 'strong-magenta',
-						'color' => '#a156b4',
-				),
-				array(
-						'name' => __( 'light grayish magenta', 'themeLangDomain' ),
-						'slug' => 'light-grayish-magenta',
-						'color' => '#d0a5db',
-				),
-				array(
-						'name' => __( 'very light gray', 'themeLangDomain' ),
-						'slug' => 'very-light-gray',
-						'color' => '#eee',
-				),
-				array(
-						'name' => __( 'very dark gray', 'themeLangDomain' ),
-						'slug' => 'very-dark-gray',
-						'color' => '#444',
-				),
-		) );
-	}
-}
+// if ( ! function_exists( 'evstrap_child_setup_theme_supported_features' ) ) {
+// 	function evstrap_child_setup_theme_supported_features() {
+// 		add_theme_support( 'editor-color-palette', array(
+// 				array(
+// 						'name' => __( 'strong magenta', 'themeLangDomain' ),
+// 						'slug' => 'strong-magenta',
+// 						'color' => '#a156b4',
+// 				),
+// 				array(
+// 						'name' => __( 'light grayish magenta', 'themeLangDomain' ),
+// 						'slug' => 'light-grayish-magenta',
+// 						'color' => '#d0a5db',
+// 				),
+// 				array(
+// 						'name' => __( 'very light gray', 'themeLangDomain' ),
+// 						'slug' => 'very-light-gray',
+// 						'color' => '#eee',
+// 				),
+// 				array(
+// 						'name' => __( 'very dark gray', 'themeLangDomain' ),
+// 						'slug' => 'very-dark-gray',
+// 						'color' => '#444',
+// 				),
+// 		) );
+// 	}
+// }
 
-add_action( 'after_setup_theme', 'evstrap_child_setup_theme_supported_features' );
+// add_action( 'after_setup_theme', 'evstrap_child_setup_theme_supported_features' );
